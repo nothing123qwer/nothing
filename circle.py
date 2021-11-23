6a24649b179c144b7562197dccf06b1a145fbfa6
nothing123qwer <15158266502@163.com>
2021-11-23 18:56:30 +0800

    learn

---
+++ circle.py
@@ -0,0 +1,30 @@
+# 绘制奥运五环
+import turtle
+turtle.width(10)
+turtle.color("blue")
+turtle.circle(50)
+turtle.color("black")
+turtle.penup()
+turtle.goto(110, 0)
+turtle.pendown()
+turtle.circle(50)
+turtle.color("red")
+turtle.penup()
+turtle.goto(220, 0)
+turtle.pendown()
+turtle.circle(50)
+turtle.color("yellow")
+turtle.penup()
+turtle.goto(55, -55)
+turtle.pendown()
+turtle.circle(50)
+turtle.color("green")
+turtle.penup()
+turtle.goto(165, -55)
+turtle.pendown()
+turtle.circle(50)
+turtle.done()
+
+
+
+
---
+++ test.py
@@ -0,0 +1,7 @@
+import turtle
+t = turtle.Pen()
+for x in range(360):
+    t.forward(x)
+    t.left(60)
+''' 第一次学习
+'''
